6f71bdbb07d6d8a6d72d1d7fff7d4966c74dd325
Manasij Mukherjee <manasij7479@gmail.com>
2013-03-30 17:01:07 +0530

    added sub makefile

--- ind/triangle/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cp tri.cpp ../../
-cd ../..
-make -j9
-rm tri.cpp
-mv main ind/triangle
-cd ind/triangle
-./main
--- ind/try/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp mygl.cpp ../../
-cd ../..
-make -j9
-mv main ind/try
-cd ind/try
-./main
